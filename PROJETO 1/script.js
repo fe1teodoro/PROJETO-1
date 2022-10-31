78a9d30f9629b677f0f92addc3a82d0124667f73
Felipe Teodoro <felipe.teodoro@ifood.com.br>
2022-10-31 00:19:32 -0300

    Validação dos campos de entrada

--- PROJETO 1/script.js
+++ PROJETO 1/script.js
@@ -2,8 +2,14 @@ const botaoBuscar = document.querySelector('.sendBuscar');
 const botaoLogar = document.querySelector('.sendLogar');
 const botaoDeslogar = document.querySelector('.logout');
 const email = document.querySelector('.loginEmail');
+const tituloEmail = document.querySelector('.emailText');
+const tituloSenha = document.querySelector('.senhaText');
 const password = document.querySelector('.loginSenha');
 const container = document.querySelector('.containerAdvice');
+let palavra = document.querySelector('.palavraChave');
+let validaEmail = false;
+let validaSenha = false;
+
 
 const login = {
     email: 'eve.holt@reqres.in',
@@ -14,6 +20,7 @@ let li;
 let ul;
 
 botaoLogar.addEventListener('click', async () =>{
+if(validaEmail && validaSenha){
 users = {
     email: email.value,
     password: password.value,
@@ -34,11 +41,13 @@ console.log(users)
 localStorage.token = users['token'];
 buttonSearch.className = 'green';
 dialogLogin.close()
+}else{
+    alert('Preencha os campos corretamente!');
+}
 });
 
 botaoBuscar.addEventListener('click', () => {
     if(localStorage.token == '"QpwL5tke4Pnpja7X4"'){
-        let palavra = document.querySelector('.palavraChave');
         fetch(`https://api.adviceslip.com/advice/search/${palavra.value}`)
         .then(resp => {
             return resp.json();
@@ -51,6 +60,7 @@ botaoBuscar.addEventListener('click', () => {
                 ul.appendChild(li);
             }
         });
+        palavra.value = '';
     }else{
         li = document.createElement('li');
         li.innerHTML = 'Usuário Deslogado';
@@ -62,6 +72,30 @@ botaoDeslogar.addEventListener('click', () => {
     buttonSearch.className = 'search';
     localStorage.removeItem('token');
 })
+
+email.addEventListener('keyup', () => {
+    if(email.value.length <= 2){
+        tituloEmail.setAttribute('style', 'color: red');
+        tituloEmail.innerHTML = '<strong>Email (Insira no mínimo 3 caracteres)<strong>';
+        validaEmail = false;
+    }else{
+        tituloEmail.setAttribute('style', 'color:black');
+        tituloEmail.innerHTML = 'Email';
+        validaEmail = true;
+    }
+});
+password.addEventListener('keyup', () => {
+    if(password.value.length <= 2){
+        tituloSenha.setAttribute('style', 'color: red');
+        tituloSenha.innerHTML = '<strong>Senha (Insira no mínimo 3 caracteres)<strong>';
+        validaSenha = false;
+    }else{
+        tituloSenha.setAttribute('style', 'color:black');
+        tituloSenha.innerHTML = 'Senha';
+        validaSenha = true;
+    }
+});
+
 
 const buttonLogin = document.querySelector('.login');
 const buttonSearch = document.querySelector('.search');
@@ -75,15 +109,18 @@ buttonLogin.addEventListener('click', () => {
 })
 
 buttonSearch.addEventListener('click', () => {
+    dialogSearch.showModal();
     ul = document.createElement('ul');
     container.appendChild(ul);
-    dialogSearch.showModal();
 })
 
 fechar.addEventListener('click', () => {
     dialogLogin.close();
+    email.value = '';
+    password.value = '';
 })
 fechar2.addEventListener('click', () => {
     dialogSearch.close();
     container.removeChild(ul);
+    palavra.value = '';
 })
