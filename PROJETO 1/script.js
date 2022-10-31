518096979c2f8ac3eb44c910c3b7c54aa055b87d
Felipe Teodoro <felipe.teodoro@ifood.com.br>
2022-10-30 22:27:10 -0300

    Atualização do código

--- PROJETO 1/script.js
+++ PROJETO 1/script.js
@@ -5,22 +5,22 @@ const email = document.querySelector('.loginEmail');
 const password = document.querySelector('.loginSenha');
 const container = document.querySelector('.containerAdvice');
 
-
 const login = {
     email: 'eve.holt@reqres.in',
     password: 'whatever'
 }
 let users;
-let li = document.createElement('li');
+let li;
+let ul;
 
-botaoLogar.addEventListener('click', () =>{
+botaoLogar.addEventListener('click', async () =>{
 users = {
     email: email.value,
     password: password.value,
     token: ''
     }
     
-fetch('https://reqres.in/api/login', {
+let data = await (fetch('https://reqres.in/api/login', {
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
@@ -28,12 +28,12 @@ fetch('https://reqres.in/api/login', {
     method: 'POST',
     body: JSON.stringify(login)
 })
-.then((response) => response.json())
-.then(function(data){
-    users['token'] = JSON.stringify(data['token']);
-    console.log(users);
-    localStorage.token = users['token'];
-})
+.then((response) => response.json()))
+users['token'] = JSON.stringify(data['token']);
+console.log(users)
+localStorage.token = users['token'];
+buttonSearch.className = 'green';
+dialogLogin.close()
 });
 
 botaoBuscar.addEventListener('click', () => {
@@ -46,41 +46,44 @@ botaoBuscar.addEventListener('click', () => {
         .then(dataAdvice => {
             let advice = dataAdvice.slips;
             for(let i = 0; i<advice.length;i++){
+                li = document.createElement('li');
                 li.innerHTML = advice[i].advice;
-                container.appendChild(li);
+                ul.appendChild(li);
             }
         });
     }else{
+        li = document.createElement('li');
         li.innerHTML = 'Usuário Deslogado';
-        container.appendChild(li);
+        ul.appendChild(li);
     };
 });
 
 botaoDeslogar.addEventListener('click', () => {
+    buttonSearch.className = 'search';
     localStorage.removeItem('token');
 })
 
-
 const buttonLogin = document.querySelector('.login');
-const buttonRegistrar = document.querySelector('.cadastro');
+const buttonSearch = document.querySelector('.search');
 const dialogLogin = document.querySelector('.dialogLogin');
-const dialogRegistrar = document.querySelector('.dialogRegistrar');
+const dialogSearch = document.querySelector('.dialogSearch');
 const fechar = document.querySelector('.fechar');
 const fechar2 = document.querySelector('.fechar2');
 
 buttonLogin.addEventListener('click', () => {
-    dialogLogin.showModal()
+    dialogLogin.showModal();
 })
 
-buttonRegistrar.addEventListener('click', () => {
-    dialogRegistrar.showModal();
+buttonSearch.addEventListener('click', () => {
+    ul = document.createElement('ul');
+    container.appendChild(ul);
+    dialogSearch.showModal();
 })
 
 fechar.addEventListener('click', () => {
-    dialogLogin.close()
+    dialogLogin.close();
 })
 fechar2.addEventListener('click', () => {
-    dialogRegistrar.close()
+    dialogSearch.close();
+    container.removeChild(ul);
 })
-
-
