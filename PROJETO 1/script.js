9c1679ab1c98cb903bfc1ab5d1b7538a5313e72c
Andrei Rezende <andrei.rezende@hotmail.com>
2022-10-30 21:12:22 -0300

    API Login e Conselho

--- PROJETO 1/script.js
+++ PROJETO 1/script.js
@@ -1,3 +1,65 @@
+const botaoBuscar = document.querySelector('.sendBuscar');
+const botaoLogar = document.querySelector('.sendLogar');
+const botaoDeslogar = document.querySelector('.logout');
+const email = document.querySelector('.loginEmail');
+const password = document.querySelector('.loginSenha');
+const container = document.querySelector('.containerAdvice');
+
+
+const login = {
+    email: 'eve.holt@reqres.in',
+    password: 'whatever'
+}
+let users;
+let li = document.createElement('li');
+
+botaoLogar.addEventListener('click', () =>{
+users = {
+    email: email.value,
+    password: password.value,
+    token: ''
+    }
+    
+fetch('https://reqres.in/api/login', {
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(login)
+})
+.then((response) => response.json())
+.then(function(data){
+    users['token'] = JSON.stringify(data['token']);
+    console.log(users);
+    localStorage.token = users['token'];
+})
+});
+
+botaoBuscar.addEventListener('click', () => {
+    if(localStorage.token == '"QpwL5tke4Pnpja7X4"'){
+        let palavra = document.querySelector('.palavraChave');
+        fetch(`https://api.adviceslip.com/advice/search/${palavra.value}`)
+        .then(resp => {
+            return resp.json();
+        })
+        .then(dataAdvice => {
+            let advice = dataAdvice.slips;
+            for(let i = 0; i<advice.length;i++){
+                li.innerHTML = advice[i].advice;
+                container.appendChild(li);
+            }
+        });
+    }else{
+        li.innerHTML = 'Usuário Deslogado';
+        container.appendChild(li);
+    };
+});
+
+botaoDeslogar.addEventListener('click', () => {
+    localStorage.removeItem('token');
+})
+
 
 const buttonLogin = document.querySelector('.login');
 const buttonRegistrar = document.querySelector('.cadastro');
